2995251e3e026a4cae4387c9d9133eed1dc34854
dryluigi <marioprasetyamulya85@gmail.com>
2024-07-24 20:53:53 +0700

    feat(auth): mock login logout feature

--- src/layouts/ECommerce.jsx
+++ src/layouts/ECommerce.jsx
@@ -1,23 +1,27 @@
+import { useState } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { Link, Outlet } from 'react-router-dom';
 
 const ECommerce = () => {
-  return (
-    <>
-        <Navbar bg="primary" variant="dark" expand="lg">
-            <Container>
-                <Link to="/"><Navbar.Brand href="#home">Toko Loak</Navbar.Brand></Link>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
-                        <Nav.Link href="#link">Login</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    return (
+        <>
+            <Navbar bg="primary" variant="dark" expand="lg">
+                <Container>
+                    <Link to="/"><Navbar.Brand href="#home">Toko Loak</Navbar.Brand></Link>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            {isLoggedIn && <Nav.Link onClick={() => setIsLoggedIn(false)}>Logout</Nav.Link>}
+                            {!isLoggedIn && <Nav.Link onClick={() => setIsLoggedIn(true)}>Login</Nav.Link>}
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
-        <Outlet />
-    </>
-  );
+            <Outlet />
+        </>
+    );
 };
 
 export default ECommerce;
